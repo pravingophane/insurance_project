e8c79c286c0339434615ab6d4f41715a9ea5dc67
unknown <pravingophane@gmail.com>
2022-10-21 13:53:15 +0530

    Added all files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medical_insurance1
+* Problem_Statement
+To Predict Premium of medical insurance
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = "Enter_path"
+
+PORT_NUMBER = 5101
+
+MODEL_FILE_PATH = os.path.join("Medical_insurance","Life_Ins_premium.pkl")
+
+JSON_FILE_PATH = os.path.join("Medical_insurance","project_data.json")
